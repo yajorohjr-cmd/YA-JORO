23f9e1eebce4c0a4b30d5a4b2b9ab4ce5811cb12
yajorohjr-cmd <yajorohjr@gmail.com>
2026-06-08 16:47:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const JAMENDO_CLIENT_ID = '7b33265e';
+const JAMENDO_CLIENT_ID = '7b33265e'; // Ilay Client ID-nao miasa
 
 app.get('/', async (req, res) => {
     const query = req.query.q || 'gasy';
-    let tracksHtml = '';
+    let wmlTracks = '';
 
     try {
         const response = await axios.get('https://api.jamendo.com/v3.0/tracks/', {
             params: {
                 client_id: JAMENDO_CLIENT_ID,
                 format: 'json',
-                limit: 10,
+                limit: 8,
                 search: query
             }
         });
@@ -23,52 +23,42 @@ app.get('/', async (req, res) => {
         if (tracks && tracks.length > 0) {
             for (let i = 0; i < tracks.length; i++) {
                 let track = tracks[i];
+                // Ovaina ho HTTP tsotra ary sivanina ny marika '&' ho an'ny WAP
                 let httpAudio = track.audio.replace("https://", "http://");
                 let safeAudioUrl = httpAudio.replace(/&/g, "&amp;");
                 
-                // HTML tsotra be tsy misy CSS mavesatra
-                tracksHtml += '<p>' +
-                              '<b>' + track.name + '</b><br/>' +
-                              'Artiste: ' + track.artist_name + '<br/>' +
-                              '<a href="' + safeAudioUrl + '">[ TELECHARGER ]</a>' +
-                              '</p><hr/>';
+                // Kaody WML madio (tahaka ny Waptrick)
+                wmlTracks += '<p>' + (i+1) + '. <b>' + track.name + '</b><br/>' +
+                             'Artiste: ' + track.artist_name + '<br/>' +
+                             '<a href="' + safeAudioUrl + '">[ TELECHARGER ]</a>' +
+                             '</p>';
             }
         } else {
-            tracksHtml = '<p>Tsy misy hira hita.</p>';
+            wmlTracks = '<p>Tsy misy hira hita.</p>';
         }
     } catch (error) {
-        tracksHtml = '<p>Nisy olana kely.</p>';
+        wmlTracks = '<p>Nisy olana kely ny fikarohana.</p>';
     }
 
-    // NY MAJIKA: Terena ho "text/html" tsotra fa tsy "xhtml+xml" mba tsy hanao Erreur Inconnu intsony
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    // TERENA HO WML MIVANTANA (WAP OFISIALY FAHIZAY)
+    res.setHeader('Content-Type', 'text/vnd.wap.wml; charset=utf-8');
     
-    // Nesorina koa ilay andalana <?xml version...?> teo aloha satria misy finday kely tsy mahazaka azy
-    let htmlResponse = '<html><head><title>WapHira Gasy</title></head>' +
-    '<body bgcolor="#003366" text="#ffffff">' +
-    '<center>' +
-    '<h2>WapHira Gasy</h2>' +
-    '<font size="2">Jamendo MP3 WAP</font>' +
-    '<br/><br/>' +
-    '<form action="/" method="get">' +
-    'Tadiavina: <input type="text" name="q" value="' + (query === 'gasy' ? '' : query) + '" size="10"/>' +
-    '<input type="submit" value="OK"/>' +
-    '</form>' +
-    '</center>' +
-    '<br/>' +
-    '<font color="#000000">' +
-    '<table bgcolor="#ffffff" width="100%"><tr><td>' +
-    '<b>Hira azo alaina:</b><br/>' +
-    tracksHtml +
-    '</td></tr></table>' +
-    '</font>' +
-    '<br/>' +
-    '<center><font size="1">&copy; 2026 - Kelitech</font></center>' +
-    '</body></html>';
+    let wmlResponse = '<?xml version="1.0" encoding="utf-8"?>' +
+    '<!DOCTYPE wml PUBLIC "-//WAPFORUM//DTD WML 1.1//EN" "http://www.wapforum.org/DTD/wml_1.1.xml">' +
+    '<wml><card id="main" title="WapHira Gasy">' +
+    '<p align="center"><b>WapHira Gasy</b><br/><small>Jamendo WML Engine</small></p>' +
+    '<p align="center">' +
+    'Tadiavina: <input name="q" size="10"/>' +
+    '<anchor>[ OK ]<go href="/" method="get"><postfield name="q" value="$(q)"/></go></anchor>' +
+    '</p>' +
+    '<p><b>Hira azo alaina:</b></p>' +
+    wmlTracks +
+    '<p align="center"><small>&copy; 2026 Kelitech</small></p>' +
+    '</card></wml>';
 
-    res.send(htmlResponse);
+    res.send(wmlResponse);
 });
 
 app.listen(PORT, () => {
-    console.log('Server running');
+    console.log('WML Server running');
 });
